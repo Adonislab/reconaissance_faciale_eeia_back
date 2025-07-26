012f27ff502388c85d023077133f92b20b49c81d
NOBIME Tanguy Adonis <nobimetanguy19@gmail.com>
2025-07-26 19:59:26 +0100

    Ajout de fichier pour la reconnaissance en temps réel

---
+++ realtime.py
@@ -0,0 +1,27 @@
+# capture_and_recognize.py
+import cv2
+import requests
+
+def capture_and_recognize():
+    cap = cv2.VideoCapture(0)
+    print("📸 Capture en cours...")
+    ret, frame = cap.read()
+    cap.release()
+
+    if not ret:
+        print("Erreur lors de la capture.")
+        return
+
+    temp_path = "temp_face.jpg"
+    cv2.imwrite(temp_path, frame)
+
+    with open(temp_path, "rb") as img_file:
+        response = requests.post(
+            "http://127.0.0.1:8000/realtime_recognize",
+            files={"file": img_file}
+        )
+
+    print("🔍 Résultat:", response.json())
+
+if __name__ == "__main__":
+    capture_and_recognize()
